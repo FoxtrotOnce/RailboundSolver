ed277075a70d60b9bf37ec2f8ca6c0f6a1ec0d38
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-06-22 07:41:35 +0700

    feat: restructure project with TypeScript configuration and React integration
    - Added main entry point for React application in `main.tsx`.
    - Removed obsolete `simulation.ts` and `sprites.ts` files.
    - Introduced new TypeScript configuration files: `tsconfig.app.json`, `tsconfig.node.json`, and updated `tsconfig.json` for better project structure.
    - Migrated Vite configuration from JavaScript to TypeScript in `vite.config.ts` and integrated React plugin.

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080,
-    open: true,
-  },
-});
+  plugins: [react()],
+})
